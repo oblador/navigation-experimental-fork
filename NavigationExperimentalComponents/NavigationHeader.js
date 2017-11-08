a329f84966e88bd0c0c0458575c9d7eb3e6e7bf0
Joel Arvidsson <joel@oblador.se>
2017-11-08 20:06:33 +0100

    Use ViewPropTypes instead of View.propTypes

--- NavigationExperimentalComponents/NavigationHeader.js
+++ NavigationExperimentalComponents/NavigationHeader.js
@@ -40,7 +40,7 @@ import React, { Component } from 'react';
 
 // const TVEventHandler = require('TVEventHandler');
 
-import { Animated, Platform, StyleSheet, View, TVEventHandler } from 'react-native';
+import { Animated, Platform, StyleSheet, View, ViewPropTypes, TVEventHandler } from 'react-native';
 
 import type {
   NavigationSceneRendererProps,
@@ -105,9 +105,9 @@ class NavigationHeader extends React.PureComponent<DefaultProps, Props, any> {
     renderLeftComponent: PropTypes.func,
     renderRightComponent: PropTypes.func,
     renderTitleComponent: PropTypes.func,
-    style: View.propTypes.style,
+    style: ViewPropTypes.style,
     statusBarHeight: PropTypes.number,
-    viewProps: PropTypes.shape(View.propTypes),
+    viewProps: PropTypes.shape(ViewPropTypes),
   };
 
   _tvEventHandler: TVEventHandler;
